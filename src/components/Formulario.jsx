f61d2cb57c42bc210b269a0915ca60e1310c1570
Adrian/Alpuche <71710102+Adrianalpuche@users.noreply.github.com>
2023-09-27 23:22:16 -0600

    update

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,23 +1,34 @@
 import React from "react";
+import { useNavigate} from 'react-router-dom';
+
 
 function Formulario () {
+    const navigate = useNavigate();
+
+    const handleLogin = async () => {
+        navigate('/', { replace: true });
+    };
+
     return (
         <div className="px-10 py-20 bg-white border-2 border-gray-100 rounded-3xl">
+            <form onSubmit={handleLogin}>
             <h1 className="text-5xl font-semibold">Welcome Back</h1>
             <p className="mt-4 text-lg font-medium text-gray-400">Welcome back! please enter your details. </p>
             <div className="mt-8">
                 <label className="text-lg font-medium">Email</label>
                 <input
-                    className = 'w-full p-4 mt-1 bg-transparent border-2 border-gray-100 rounded-x'
+                    className = 'w-full p-4 mt-1 bg-transparent border-2 border-gray-100 rounded-xl'
                     placeholder = 'Enter your email'
+                    required
                 />
             </div>
             <div>
-                <label>Password</label>
+                <label className="text-lg" >Password</label>
                 <input
-                    className = 'w-full p-4 mt-1 bg-transparent border-2 border-gray-100 rounded-x'
+                    className = 'w-full p-4 mt-1 bg-transparent border-2 border-gray-100 rounded-xl'
                     placeholder = 'Enter your password'
                     type = "password" 
+                    required
 
                 />
             </div>
@@ -31,8 +42,9 @@ function Formulario () {
             <div className="flex items-center justify-center mt-8">
                 <p className="text-base font-medium"> Don´t have a account </p>
                 <button className='ml-2 text-base font-medium text-violet-400'>Sign up</button>
-
+                
             </div>
+            </form>
         </div>
     )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -10,17 +10,13 @@ function Login ()  {
                 <Formulario/>
             </div>
             <div className="items-center justify-center hidden w-1/2 h-full bg-gray-200 lg:flex">
-            <div
-            className="w-full h-full rounded-lg"
-            
-          >
+                <div className="w-full h-full rounded-lg" >
             <img
               src="https://imageio.forbes.com/specials-images/imageserve/642bc7aea9b99c12f3deb166/0x0.jpg?format=jpg&width=1200"
               alt="Imagen"
               className="object-cover w-full h-full rounded-lg"
             />
-        </div>
-                
+                </div>
             </div>
         </div>
         
